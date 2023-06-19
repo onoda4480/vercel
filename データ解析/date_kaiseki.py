6fe5618bd4e3e0ea9debb8657a4f955c56a24276
onoda4480 <123041906+onoda4480@users.noreply.github.com>
2023-06-19 18:19:22 +0900

    Delete データ解析 directory

--- データ解析/date_kaiseki.py
+++
@@ -1,8 +0,0 @@
-import pandas as pd
-df = pd.read_csv(
-header=None,
-sep='¥s+')
-df.columns = ['CRIM', 'ZN', 'INDUS', 'CHAS', 
-'NOX', 'RM', 'AGE', 'DIS', 'RAD', 
-'TAX', 'PTRATIO', 'B', 'LSTAT', 'MEDV']
-df.head()
