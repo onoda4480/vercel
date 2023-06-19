c7742aa25ea693e174640f2edb50de3db0f109b2
onoda4480 <123041906+onoda4480@users.noreply.github.com>
2023-06-19 18:18:17 +0900

    Delete datekaiseki directory

--- datekaiseki/date_kaiseki.py
+++
@@ -1,8 +0,0 @@
-import pandas as pd
-df = pd.read_csv('datekaiseki\housing.data.txt',
-header=None,
-sep='¥s+')
-df.columns = ['CRIM', 'ZN', 'INDUS', 'CHAS', 
-'NOX', 'RM', 'AGE', 'DIS', 'RAD', 
-'TAX', 'PTRATIO', 'B', 'LSTAT', 'MEDV']
-df.head()
