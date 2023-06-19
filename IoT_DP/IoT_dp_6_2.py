6d1e1291144e0a67490d3666152c194b21187f76
onoda4480 <123041906+onoda4480@users.noreply.github.com>
2023-06-19 16:10:59 +0900

    Delete IoT_DP directory

--- IoT_DP/IoT_dp_6_2.py
+++
@@ -1,8 +0,0 @@
-import pandas as pd
-dat_df = pd.read_csv('energydata.csv',sep=',')
-dat_df.head()
-print(dat_df.shape)
-print(dat_df.dtypes)
-
-dat_df['date'] = pd.to_datetime(dat_df['date'],format = '%Y-%m-%d %H:%M:%S')
-print(dat_df['date'])
